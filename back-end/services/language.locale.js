efd6207c0c73ded965b6e70c7c83dbbc4573c361
canino_jories <canino_jories@hotmail.com>
2015-03-23 13:15:26 +0800

    adding condition if the language is not yet present

--- back-end/services/language.locale.js
+++ back-end/services/language.locale.js
@@ -4,9 +4,10 @@
   var i18n = require('i18n');
 
   module.exports = function(req, res, next) {
-    var lang = req.headers.language.split(' ')[1];
-    res.setLocale(lang);
-
+    if(!req.headers.language) {
+      var lang = req.headers.language.split(' ')[1];
+      res.setLocale(lang);
+    }
     next();
   };
 }());
