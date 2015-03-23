559504bcb4c5e5fa0c35cd36ee738e589c9bd554
canino_jories <canino_jories@hotmail.com>
2015-03-23 13:56:25 +0800

    adding mongoose connection mongolab

--- back-end/configuration/mongodb.js
+++ back-end/configuration/mongodb.js
@@ -3,6 +3,8 @@
 
   module.exports = function (dbName) {
     if (io.mongoose.connection.readyState === 0) {
+      console.log('mongolab uri');
+      console.log(process.env.MONGOLAB_URI);
       return io.mongoose.connectAsync(process.env.MONGOLAB_URI || dbName);
     } else {
       return io.mongoose.disconnectAsync(function() {
