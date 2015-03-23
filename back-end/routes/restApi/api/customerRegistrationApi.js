16e571ad45a5408db36127ab41eaf2e0db2b72bf
canino_jories <canino_jories@hotmail.com>
2015-03-23 12:31:29 +0800

    adding deleted files in renaming folder

---
+++ back-end/routes/restApi/api/customerRegistrationApi.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  var express = require('express'),
+      app     = express(),
+
+      POST_CustomerRegistration = require('../adminApImplementation/customerRegistration/postIndex.js'),
+      GET_CustomerRegistration  = require('../adminApImplementation/customerRegistration/getIndex.js');
+
+    app.route('/customerRegistration')
+      .get(GET_CustomerRegistration.getOne)
+      .post(POST_CustomerRegistration.post);
+
+    module.exports = app;
+}());
---
+++ back-end/routes/restApi/api/salesRepresentativeRegistrationApi.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+    var express   = require('express'),
+    router        = express.Router(),
+    app           = express(),
+
+    postRegisterSalesRepresentative = require('../adminApImplementation/salesRepresentativeRegistration/postIndex.js');
+
+    app.route('/saveSalesRepresentative')
+    .post(postRegisterSalesRepresentative.saveSalesRepresentative);
+
+    module.exports = app;
+}());
