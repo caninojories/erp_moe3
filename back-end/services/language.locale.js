8530dcb864242c472195f9c91c829a33596c2779
canino_jories <canino_jories@hotmail.com>
2015-03-23 13:17:49 +0800

    adding condition for req.headers.language

--- back-end/services/language.locale.js
+++ back-end/services/language.locale.js
@@ -4,7 +4,7 @@
   var i18n = require('i18n');
 
   module.exports = function(req, res, next) {
-    if(!req.headers.language) {
+    if(req.headers.language) {
       var lang = req.headers.language.split(' ')[1];
       res.setLocale(lang);
     }
