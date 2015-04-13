83e1821ec8c425e0ddd1f8e300e8ba7985447ea9
canino_jories <canino_jories@hotmail.com>
2015-04-13 14:05:07 +0800

    refactoring sales registration html

--- back-end/routes/admin/salesRepresentativeRegistration/index.js
+++ back-end/routes/admin/salesRepresentativeRegistration/index.js
@@ -5,9 +5,10 @@
   var express = require('express'),
   router   = express.Router();
 
-  router.get('/admin/salesRepresentativeRegistration/index.html', function(req, res) {
-    res.render('admin/salesRepresentativeRegistration/index.html', {title: 'salesRepresentativeRegistration'});
-  });
+  router.get('/admin/salesRepresentativeRegistration/index.html', io.xPoweredBy, io.languageLocale, io.authorize,
+    function(req, res) {
+      res.render('admin/salesRepresentativeRegistration/index.html', {title: 'salesRepresentativeRegistration'});
+    });
 
   module.exports = router;
 }());
